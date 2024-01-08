399cb1436e7101092a6c099c31c3020e3bef064e
Afga Ghifari Jamaludin <ghifarij.25@gmail.com>
2024-01-09 00:22:28 +0700

    feat: add Login, SignUp, GetUsers, and GetUserByID

---
+++ middleware/authMiddleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"fmt"
+	helper "github.com/ghifarij/golang-jwt-project/helpers"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("no authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("first_name", claims.FirstName)
+		c.Set("last_name", claims.LastName)
+		c.Set("uid", claims.Uid)
+		c.Set("user_type", claims.UserType)
+		c.Next()
+	}
+}
---
+++ routes/userRouter.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	controller "github.com/ghifarij/golang-jwt-project/controllers"
+	"github.com/ghifarij/golang-jwt-project/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRequest *gin.Engine) {
+	incomingRequest.Use(middleware.Authenticate())
+	incomingRequest.GET("/users", controller.GetUsers())
+	incomingRequest.GET("/users/:users_id", controller.GetUserByID())
+}
